b5eda1b596c97c2a8fb939db1691ac47f6def9d7
majes <liones2014@mail.ru>
2020-11-26 12:54:02 +0700

    create routing and admin module

---
+++ app/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+
+
+@NgModule({
+    imports: [
+        CommonModule     //что бы использовать базовые дерективы, пайпы в модуле
+    ],
+    exports: []
+})
+export class AdminModule {
+
+}
