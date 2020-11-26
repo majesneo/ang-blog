7261f0ae00df6c25c1ab53d14dbbc09eb03425bb
majes <liones2014@mail.ru>
2020-11-26 14:55:11 +0700

    validations forms

--- app/admin/admin.module.ts
+++ app/admin/admin.module.ts
@@ -6,19 +6,22 @@ import {LoginPageComponent} from './login-page/login-page.component';
 import {CreatePageComponent} from './create-page/create-page.component';
 import {EditPageComponent} from './edit-page/edit-page.component';
 import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
     declarations: [AdminLayoutComponent, LoginPageComponent, CreatePageComponent, EditPageComponent, DashboardPageComponent],
     imports: [
-        CommonModule,     //что бы использовать базовые дерективы, пайпы в модуле
+        CommonModule,   //что бы использовать базовые дерективы, пайпы в модуле
+        FormsModule,    //для использования реактивных форм в модуле admin
+        ReactiveFormsModule,
         RouterModule.forChild([
             {
                 path: '', component: AdminLayoutComponent, children: [
                     {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
                     {path: 'login', component: LoginPageComponent},
                     {path: 'dashboard', component: DashboardPageComponent},
-                    {path: 'crate', component: CreatePageComponent},
+                    {path: 'create', component: CreatePageComponent},
                     {path: 'post/:id/edit', component: EditPageComponent},
                 ]
             }
--- app/admin/login-page/login-page.component.ts
+++ app/admin/login-page/login-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../shared/interfaces";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+        this.form = new FormGroup({
+            email: new FormControl(null, [
+                Validators.email,
+                Validators.required
+            ]),
+            password: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(6)
+            ]),
+        })
+    }
+
+    submit() {
+        console.log(this.form)
+        if (this.form.invalid) {
+            return
+        } else {
+            const user: User = {
+                email: this.form.value.email,
+                password: this.form.value.password
+            }
+        }
+    }
+
+    FF
 }
